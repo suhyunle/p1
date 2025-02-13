f12ee7c55df1dc2b22e1673c03f6eea41534feb0
Suhyun Lee <122947409+suhyunle@users.noreply.github.com>
2025-02-13 12:35:50 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
-export const WEDDING_DATE = "1970년 01월 01일, 목요일 오전 12시 00분";
-export const WEDDING_LOCATION = "○○○웨딩, ○층 ○○홀";
+export const WEDDING_DATE = "2028년 4월 19일, 일요일 12시 00분";
+export const WEDDING_LOCATION = "새빛 동동섬, 2층 웨딩홀";
 
 export const GROOM_NAME = "○○○";
 export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
